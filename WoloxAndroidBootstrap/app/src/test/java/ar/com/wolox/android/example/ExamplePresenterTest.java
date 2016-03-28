8c867dc0e82b9a0b2de2ccfe13eb290fcc29ad8c
Federico Ramundo <federico.ramundo@wolox.com.ar>
2016-03-28 16:42:34 -0300

    example tests

---
+++ WoloxAndroidBootstrap/app/src/test/java/ar/com/wolox/android/example/ExamplePresenterTest.java
@@ -0,0 +1,42 @@
+package ar.com.wolox.android.example;
+
+import android.content.Context;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
+import static org.mockito.Mockito.*;
+
+public class ExamplePresenterTest {
+
+    ExampleView mExampleView;
+    Context mContext;
+    ExamplePresenter mExamplePresenter;
+
+    @Before
+    public void createInstances() {
+        mExampleView = mock(ExampleView.class);
+        mContext = mock(Context.class);
+        mExamplePresenter = new ExamplePresenter(mExampleView, mContext);
+    }
+
+    @Test
+    public void randomNumberIsValid() {
+
+        int randomNumberGenerated = mExamplePresenter.generateRandomNumber();
+
+        assertThat(randomNumberGenerated, greaterThanOrEqualTo(ExamplePresenter.NUMBER_MIN));
+        assertThat(randomNumberGenerated, lessThanOrEqualTo(ExamplePresenter.NUMBER_MAX));
+    }
+
+    @Test
+    public void randomNumberUpdatesView() {
+        int randomNumberGenerated = mExamplePresenter.generateRandomNumber();
+        verify(mExampleView, times(1)).onRandomNumberUpdate(randomNumberGenerated);
+    }
+
+}
