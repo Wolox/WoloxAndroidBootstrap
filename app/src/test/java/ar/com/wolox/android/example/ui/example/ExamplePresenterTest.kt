2fd8a01ca97aaa070db74af715affa0a76179fb7
Dylan Muszel <dylan.muszel@wolox.com.ar>
2019-12-19 11:59:45 -0300

    Updating versions

--- app/src/test/java/ar/com/wolox/android/example/ui/example/ExamplePresenterTest.kt
+++ app/src/test/java/ar/com/wolox/android/example/ui/example/ExamplePresenterTest.kt
@@ -1,38 +1,36 @@
 package ar.com.wolox.android.example.ui.example
 
+import ar.com.wolox.android.example.utils.UserSession
+import org.junit.Before
+import org.junit.Test
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 
-import ar.com.wolox.android.example.utils.UserSession
-
-import org.junit.Before
-import org.junit.Test
-
 class ExamplePresenterTest {
 
-    private lateinit var mExampleView: ExampleView
-    private lateinit var mExamplePresenter: ExamplePresenter
-    private lateinit var mUserSession: UserSession
+    private lateinit var exampleView: ExampleView
+    private lateinit var examplePresenter: ExamplePresenter
+    private lateinit var userSession: UserSession
 
     @Before
     fun createInstances() {
-        mExampleView = mock(ExampleView::class.java)
-        mUserSession = mock(UserSession::class.java)
-        mExamplePresenter = ExamplePresenter(mUserSession)
+        exampleView = mock(ExampleView::class.java)
+        userSession = mock(UserSession::class.java)
+        examplePresenter = ExamplePresenter(userSession).apply {
+            attachView(exampleView)
+        }
     }
 
     @Test
-    fun usernameIsStored() {
-        mExamplePresenter.attachView(mExampleView)
-        mExamplePresenter.onLoginButtonClicked("Test")
-        verify<UserSession>(mUserSession, times(1)).username = "Test"
+    fun `when login button is clicked then user session should be updated`() {
+        examplePresenter.onLoginButtonClicked("Test")
+        verify<UserSession>(userSession, times(1)).username = "Test"
     }
 
     @Test
-    fun storeUsernameUpdatesView() {
-        mExamplePresenter.attachView(mExampleView)
-        mExamplePresenter.onLoginButtonClicked("Test")
-        verify<ExampleView>(mExampleView, times(1)).goToViewPager()
+    fun `when login button is clicked then view should go to viewpager`() {
+        examplePresenter.onLoginButtonClicked("Test")
+        verify<ExampleView>(exampleView, times(1)).goToViewPager()
     }
 }
